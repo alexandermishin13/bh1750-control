7cbcc475286cb92a5f07b5954f0f63b2bcc413cc
Александр Мишин <mishin@mh.net.ru>
2020-09-29 00:01:19 +0400

    Changes to the service script for profiles support

--- bh1750-daemon.sh
+++ bh1750-daemon.sh
@@ -12,8 +12,22 @@
 #				Set to "YES" to enable bh1750_daemon.
 # bh1750_daemon_profiles (str):	Set to "" by default.
 #				Define your profiles here.
+# bh1750_daemon_number (str):	Set to "0" by default.
+#				Set one from sysctl dev.bh1750.<number>
+# bh1750_daemon_dbfile (str):	Set to "/var/db/bh1750/actions.sqlite" by default.
+#				Set sqlite3 db file here
+# bh1750_daemon_pidfile (str):	Set to "/var/run/bh1750-daemon.pid" by default
+#				Define a pid file here.
 # bh1750_daemon_flags (str):	Set to "" by default.
 #				Extra flags passed to start command.
+#
+# For a profile based configuration use variables like this:
+#
+# bh1750_daemon_profiles="XXX"
+# bh1750_daemon_XXX_number
+# bh1750_daemon_XXX_dbfile
+# bh1750_daemon_XXX_pidfile
+# bh1750_daemon_XXX_flags
 
 . /etc/rc.subr
 
@@ -30,7 +44,43 @@ load_rc_config $name
 
 pidfile=${bh1750_daemon_pidfile}
 bh1750_daemon_bin="/usr/local/sbin/bh1750-daemon"
+
+is_profile_exists() {
+	local profile
+
+	for profile in $bh1750_daemon_profiles; do
+		[ "$profile" = "$1" ] && return 0;
+	done
+
+	return 1
+}
+
+if [ -n "${bh1750_daemon_profiles}" ]; then
+	if [ -n "$2" ]; then
+		profile="$2"
+		if ! is_profile_exists $profile; then
+			echo "$0: no such profile defined in bh1750_daemon_profiles."
+			exit 1
+		fi
+		eval pidfile=\${bh1750_daemon_${profile}_pidfile:-"/var/run/bh1750-daemon-${profile}.pid"}
+		eval bh1750_daemon_number=\${bh1750_daemon_${profile}_number:-"${bh1750_daemon_number}"}
+		eval bh1750_daemon_dbfile=\${bh1750_daemon_${profile}_dbfile:-"${bh1750_daemon_dbfile}"}
+	elif [ -n "$1" ]; then
+		for profile in ${bh1750_daemon_profiles}; do
+			echo "===> bh1750-daemon profile: ${profile}"
+			/usr/local/etc/rc.d/bh1750-daemon $1 ${profile}
+			retcode="$?"
+			if [ "0${retcode}" -ne 0 ]; then
+				failed="${profile} (${retcode}) ${failed:-}"
+			else
+				success="${profile} ${success:-}"
+			fi
+		done
+		exit 0
+	fi
+fi
+
 command=${bh1750_daemon_bin}
-command_args="-i ${bh1750_daemon_number} -f ${bh1750_daemon_dbfile} -p ${pidfile}"
+command_args="-i ${bh1750_daemon_number} -f ${bh1750_daemon_dbfile} -p ${pidfile} ${bh1750_daemon_flags}"
 
 run_rc_command "$@"
